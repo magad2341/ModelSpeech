7752d40163cc0ddf8ce53eeeb8d9a64b441b4443
asgmodel <145135565+asgmodel@users.noreply.github.com>
2025-01-19 21:41:08 +0300

    Update gradio_ui.py

--- gradio_ui.py
+++ gradio_ui.py
@@ -5,7 +5,7 @@ from gradio_client import Client
 def greet(text: str) -> str:
     client = Client("wasmdashai/RunTasking")
     result = client.predict(
-    		text="Hello!!",
+    		text=text,
     		name_model="wasmdashai/vits-ar-sa-huba-v2",
     		speaking_rate=0.8,
     		api_name="/predict"
