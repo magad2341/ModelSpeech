44dd21929e49346fe6ef4fde041c190d91a29128
asgmodel <145135565+asgmodel@users.noreply.github.com>
2025-01-19 21:11:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from gradio_client import Client
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    client = Client("wasmdashai/RunTasking")
+    result = client.predict(
+    		text="السلام عليكم كيف الحال",
+    		name_model="wasmdashai/vits-ar-sa-huba-v2",
+    		speaking_rate=0.8,
+    		api_name="/predict"
+    )
+    return {"message": result}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
