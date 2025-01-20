fe9cdd718d173c4a228cc54ff650f028fbdf7db6
asgmodel <145135565+asgmodel@users.noreply.github.com>
2025-01-21 00:10:09 +0300

    Create t2speech.py

---
+++ t2speech.py
@@ -0,0 +1,88 @@
+import gradio as gr
+from gradio_client import Client
+
+bodyicon = """
+    <style>
+      :root {
+    --name: default;
+
+    --primary-500: rgba(11, 186, 131, 1);
+    }
+      .shadow-primary {
+        box-shadow: 0 4px 8px rgba(0, 123, 255, 0.25);
+      }
+      .icon-xxl {
+        width: 170px;
+        height: 170px;
+        line-height: 6.8rem;
+        align-items: center;
+      }
+      .icon-md, .icon-lg, .icon-xl, .icon-xxl {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 50%;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        background-color: #ffffff;
+      }
+      .flex-shrink-0 {
+        flex-shrink: 0 !important;
+      }
+      .rounded-circle {
+        border-radius: 50% !important;
+      }
+      .text-center {
+        text-align: center;
+      }
+      .mud-icon-root.mud-svg-icon {
+        fill: rgba(11,186,131,1);
+      }
+      .mud-icon-size-large {
+        font-size: 4.25rem !important;
+        width: 7.25rem !important;
+        height: 7.25rem !important;
+      }
+      .mud-success-text {
+        color: rgba(11,186,131,1);
+      }
+      .icon-cont-center {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+
+      }
+      .built-with.svelte-sar7eh.svelte-sar7eh.svelte-sar7eh {
+        display:none !important;
+      }
+     footer.svelte-sar7eh.svelte-sar7eh.svelte-sar7eh {
+    position: fixed;
+    right: 20px;
+    top: 0;
+}
+       .gap.svelte-vt1mxs {
+    gap: 8px !important;
+}
+    </style>
+    <div class="icon-cont-center  ">
+    <div id="logo-icon-static-id" class="icon-xxl text-center shadow-primary rounded-circle flex-shrink-0">
+        <svg class="mud-icon-root mud-svg-icon mud-success-text mud-icon-size-large" style="direction:ltr !important;margin:8px !important" focusable="false" viewBox="0 0 24 24" aria-hidden="true" role="img">
+            <title>API</title>
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M6 13c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1zm0 4c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1zm0-8c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1zm-3 .5c-.28 0-.5.22-.5.5s.22.5.5.5.5-.22.5-.5-.22-.5-.5-.5zM6 5c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1zm15 5.5c.28 0 .5-.22.5-.5s-.22-.5-.5-.5-.5.22-.5.5.22.5.5.5zM14 7c.55 0 1-.45 1-1s-.45-1-1-1-1 .45-1 1 .45 1 1 1zm0-3.5c.28 0 .5-.22.5-.5s-.22-.5-.5-.5-.5.22-.5.5.22.5.5.5zm-11 10c-.28 0-.5.22-.5.5s.22.5.5.5.5-.22.5-.5-.22-.5-.5-.5zm7 7c-.28 0-.5.22-.5.5s.22.5.5.5.5-.22.5-.5-.22-.5-.5-.5zm0-17c.28 0 .5-.22.5-.5s-.22-.5-.5-.5-.5.22-.5.5.22.5.5.5zM10 7c.55 0 1-.45 1-1s-.45-1-1-1-1 .45-1 1 .45 1 1 1zm0 5.5c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5zm8 .5c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1zm0 4c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1zm0-8c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1zm0-4c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1zm3 8.5c-.28 0-.5.22-.5.5s.22.5.5.5.5-.22.5-.5-.22-.5-.5-.5zM14 17c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1zm0 3.5c-.28 0-.5.22-.5.5s.22.5.5.5.5-.22.5-.5-.22-.5-.5-.5zm-4-12c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5zm0 8.5c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1zm4-4.5c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5zm0-4c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5z"></path>
+        </svg>
+    </div>
+    </div>
+"""
+def  modelspeech(text=TXT,name_model="wasmdashai/vits-ar-sa-huba-v2",speaking_rate=0.8,double_duration=1.0) -> str:
+    client = Client("wasmdashai/RunTasking")
+    result = client.predict(
+    		text=text,
+    		name_model=name_model,
+    		speaking_rate=speaking_rate,
+    		api_name="/predict"
+    )
+    return bodyicon,result
+
+demo = gr.Interface(fn=modelspeech, inputs=["text",model_choices,gr.Slider(0.1, 1, step=0.1,value=0.8),gr.Slider(0.1, 5, step=0.1,value=1.0)], outputs=[ gr.HTML(bodyicon),gr.Audio(streaming=True, autoplay=True)], allow_flagging='never')
+
