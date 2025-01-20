9da249288429e66a5c7c3323fe31bce44d54d94d
asgmodel <145135565+asgmodel@users.noreply.github.com>
2025-01-21 01:15:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import gradio as gr
 
 from gradio_ui import demo
 import t2speech
-
+import t2text
 app = FastAPI()
 
 @app.get('/')
@@ -13,7 +13,7 @@ async def root():
 app = gr.mount_gradio_app(app, demo, path='/studio-t2speech')
 app = gr.mount_gradio_app(app, t2speech.demo, path='/t2speech')
 
-app = gr.mount_gradio_app(app, demo, path='/t2text')
+app = gr.mount_gradio_app(app, t2text.demo, path='/studio-t2text')
 
 app = gr.mount_gradio_app(app, demo, path='/vbot')
 
